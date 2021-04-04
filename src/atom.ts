0101cfe1d93ec5b757e755583e64e371e8b5e098
orangain <orangain@gmail.com>
2021-04-04 12:20:12 +0900

    Respect center color

--- src/atom.ts
+++ src/atom.ts
@@ -9,12 +9,13 @@ export const chakraColorAtom = atom<ThemeOverride["colors"]>((get) => {
 
   Object.entries(overrideConfig).forEach(([colorKey, config]) => {
     if (!config) return
-    const color = config.centerColor
+
     const darkSteps = 4
     const lightSteps = 5
+    const centerLightness = Color(config.centerColor).lightness()
 
-    const lightnessStep = (config.lightest.lightness - 50) / lightSteps
-    const darknessStep = (50 - config.darkest.lightness) / darkSteps
+    const lightnessStep = (config.lightest.lightness - centerLightness) / lightSteps
+    const darknessStep = (centerLightness - config.darkest.lightness) / darkSteps
 
     const lightRotateStep = config.lightest.rotate / lightSteps
     const darkRotateStep = config.darkest.rotate / darkSteps
@@ -24,48 +25,48 @@ export const chakraColorAtom = atom<ThemeOverride["colors"]>((get) => {
 
     const scale = {
       50: Color(config.centerColor)
-        .lightness(50 + lightnessStep * 5)
+        .lightness(centerLightness + lightnessStep * 5)
         .rotate(lightRotateStep * 5)
         .saturate(lightSaturateStep * 5)
         .hex(),
       100: Color(config.centerColor)
-        .lightness(50 + lightnessStep * 4)
+        .lightness(centerLightness + lightnessStep * 4)
         .rotate(lightRotateStep * 4)
         .saturate(lightSaturateStep * 4)
         .hex(),
       200: Color(config.centerColor)
-        .lightness(50 + lightnessStep * 3)
+        .lightness(centerLightness + lightnessStep * 3)
         .rotate(lightRotateStep * 3)
         .saturate(lightSaturateStep * 3)
         .hex(),
       300: Color(config.centerColor)
-        .lightness(50 + lightnessStep * 2)
+        .lightness(centerLightness + lightnessStep * 2)
         .rotate(lightRotateStep * 2)
         .saturate(lightSaturateStep * 2)
         .hex(),
       400: Color(config.centerColor)
-        .lightness(50 + lightnessStep * 1)
+        .lightness(centerLightness + lightnessStep * 1)
         .rotate(lightRotateStep * 1)
         .saturate(lightSaturateStep * 1)
         .hex(),
-      500: Color(config.centerColor).lightness(50).hex(),
+      500: Color(config.centerColor).hex(),
       600: Color(config.centerColor)
-        .lightness(50 - darknessStep * 1)
+        .lightness(centerLightness - darknessStep * 1)
         .rotate(darkRotateStep * 1)
         .saturate(darkSaturateStep * 1)
         .hex(),
       700: Color(config.centerColor)
-        .lightness(50 - darknessStep * 2)
+        .lightness(centerLightness - darknessStep * 2)
         .rotate(darkRotateStep * 2)
         .saturate(darkSaturateStep * 2)
         .hex(),
       800: Color(config.centerColor)
-        .lightness(50 - darknessStep * 3)
+        .lightness(centerLightness - darknessStep * 3)
         .rotate(darkRotateStep * 3)
         .saturate(darkSaturateStep * 3)
         .hex(),
       900: Color(config.centerColor)
-        .lightness(50 - darknessStep * 4)
+        .lightness(centerLightness - darknessStep * 4)
         .rotate(darkRotateStep * 4)
         .saturate(darkSaturateStep * 4)
         .hex(),
